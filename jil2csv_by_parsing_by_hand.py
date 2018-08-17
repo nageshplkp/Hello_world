54fa982ba609b05d1f348c3dbf623388984f8fa8
samc0de <sssam.sam@gmail.com>
2018-08-18 00:23:20 +0530

    Update jil2csv_by_parsing_by_hand.py

--- jil2csv_by_parsing_by_hand.py
+++ jil2csv_by_parsing_by_hand.py
@@ -1,9 +1,29 @@
 # coding: utf-8
 import csv
+import re
+import sys
 
 
-with open('example.jil', 'rt') as fl:
-    code = fl.read()
+def remove_comments(string):
+    # Streamed comments (/*COMMENT */)
+    string = re.sub(re.compile("/\*.*?\*/",re.DOTALL ) ,"" ,string)
+    # Singleline comments (//COMMENT\n )
+    string = re.sub(re.compile("//.*?\n" ) ,"" ,string)
+    return string
+
+
+# with open('example.jil', 'rt') as fl:
+#     code = fl.read()
+
+
+jil_config = ''
+
+with open(sys.argv[1], 'rt') as file_obj:
+    jil_config = file_obj.read()
+
+
+# Remove comments.
+config = remove_comments(jil_config)
 
 
 def get_jobs(config_str):
@@ -11,38 +31,37 @@ def get_jobs(config_str):
     lines = config_str.splitlines()
     start_header = 'insert_job'
     started = False
+    job_dict = {}
     for line in lines:
         if not line:
+            continue
+        entry_count = line.count(separator)
+        if not entry_count:
             continue
         if line.strip().startswith(start_header):
             if started:
                 jobs.append(job_dict)
-            entry_count = line.count(separator)
-            if entry_count == 0:
-                continue
-            if entry_count == 1:
-                job_dict = dict([x.strip() for x in line.split(separator)])
-            if entry_count > 1:
-                entries = line.split(separator)
-                inners = [y.strip() for x in entries[1:-1] for y in x.split()]
-                texts = [entries[0]] + inners + [entries[-1]]
-                job_dict = dict(texts[i:i + 2] for i in range(0, len(texts), 2))
+            job_dict = {}
             started = True
-        try:
+        if entry_count == 1:
             key, value = [x.strip() for x in line.split(separator)]
-        except ValueError as e:
-            continue
-        if started:
-            job_dict[key] = value
-    jobs.append(job_dict)
+            job_dict.update({key: value})
+        elif entry_count > 1:
+            entries = line.split(separator)
+            inners = [y.strip() for x in entries[1:-1] for y in x.split()]
+            texts = [entries[0].strip()] + inners + [entries[-1].strip()]
+            job_dict.update(dict(texts[i:i + 2] for i in range(0, len(texts), 2)))
+
+    if job_dict:
+        jobs.append(job_dict)
     return jobs
 
-jobs = get_jobs(new_code)
+jobs = get_jobs(config)
 # You can hardcode these if you need any specific order.
 cols = set(sum((x.keys() for x in jobs), []))
 
 
-with open('output.csv', 'wb') as f:
+with open('output_1.csv', 'wb') as f:
     writer = csv.DictWriter(f, cols)
     writer.writeheader()
     writer.writerows(jobs)
