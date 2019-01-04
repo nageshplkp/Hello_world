911c21709cbf1da2cf7b6a148cc5add982bad5f2
nageshplkp <30627936+nageshplkp@users.noreply.github.com>
2019-01-04 19:06:03 +0530

    Update pro_result.py

--- swwamy/pro_result.py
+++ swwamy/pro_result.py
@@ -19,12 +19,12 @@ def main_run(self):
 					response['request_status'], payload))
 		else:
 			n = 500
-      chunk_total = list(divide_chunks(self.securities, n))
-      response_list = []
-      for total in chunk_total:
-          payload = self.gen_request_object(total, self.mnemonics)
-			    response = self.post_to_bt(payload)
-          response_list.extend(response)
+      			chunk_total = list(divide_chunks(self.securities, n))
+      			response_list = []
+      			for total in chunk_total:
+          			payload = self.gen_request_object(total, self.mnemonics)
+			    	response = self.post_to_bt(payload)
+          			response_list.extend(response)
 			# here we need to change response as response_list or vice-versa , depends on getting the result 
 			if response['request_status'] == self.success and response['data_file_path']:
 				dst_file_path = self.copy_file(response['data_file_path'])
